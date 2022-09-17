0f747430c2bca547ef243cb1351d1230cc13f65d
abha <abhaaibrahim@gmail.com>
2022-09-17 09:48:32 +0500

    wip

---
+++ components/Text.tsx
@@ -0,0 +1,37 @@
+import { useScroll, motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+
+type Props = {
+  text: string;
+  secondaryText?: string;
+  scrollPosition?: number
+};
+
+const Text = ({ text, secondaryText, scrollPosition }: Props) => {
+  const ref = useRef<HTMLParagraphElement>(null);
+
+  const { scrollY } = useScroll({
+    target: ref,
+  });
+
+  console.log(scrollY)
+
+  return (
+    <motion.p
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1  }}
+      viewport={{
+        once: false,
+        amount: 1,
+      }}
+      className="text-white font-medium text-7xl"
+      ref={ref}
+    >
+      {text}
+      {secondaryText && <br />}
+      {secondaryText}.
+    </motion.p>
+  );
+};
+
+export default Text;
